054317fa40a5bc3a8b006f03f4fb0398bc09a1b7
Bianca Danforth <biancadanforth@gmail.com>
2021-01-26 17:06:54 -0500

    feat(payments): Add PayPal handlers to button on Subscribe page

--- packages/fxa-payments-server/src/routes/Product/PayPalButton/index.tsx
+++ packages/fxa-payments-server/src/routes/Product/PayPalButton/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { useCallback } from 'react';
+
 declare var paypal: {
   Buttons: {
     driver: Function;
@@ -13,7 +15,35 @@ const PaypalButtonBase = paypal.Buttons.driver('react', {
 });
 
 export const PaypalButton = () => {
-  return <PaypalButtonBase data-testid="paypal-button" />;
+  const createOrder = useCallback((data: any, actions: any) => {
+    console.log('CANARY', 'createOrder', data, actions);
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: '0.01',
+          },
+        },
+      ],
+    });
+  }, []);
+  const onApprove = useCallback((data: any, actions: any) => {
+    console.log('CANARY', 'onApprove', data, actions);
+    return actions.order.capture();
+  }, []);
+
+  // POST to the fxa-auth-server to create a new Stripe customer from Stripe if one doesn’t exist.
+  //   - TODO: Above may already happen; verify
+  // POST to fxa-auth-server to get a checkout token from PayPal
+  //   - Route: "/oauth/subscription/paypal-checkout"
+
+  return (
+    <PaypalButtonBase
+      data-testid="paypal-button"
+      createOrder={createOrder}
+      onApprove={onApprove}
+    />
+  );
 };
 
 export default PaypalButton;
